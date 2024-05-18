16a703ba87a70e9c000a55e7c513965329da9f14
Doğuhan İlter <doguhannilt@gmail.com>
2024-05-18 12:20:27 +0300

    map function is added, skilledBlog is rendered

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,56 +1,41 @@
 import React from 'react'
+
 import {
   Table,
   Thead,
   Tbody,
-  Tfoot,
   Tr,
   Th,
   Td,
-  TableCaption,
   TableContainer,
 } from '@chakra-ui/react'
+
+import { skilledBlog } from '../Content/blogs';
+
+
 const Skills = () => {
   return (
-    <div>
-      <TableContainer>
-  <Table variant='striped' colorScheme='teal'>
-    <TableCaption></TableCaption>
-    <Thead>
-      <Tr>
-        <Th>Skills</Th>
-        <Th>Decription</Th>
-        <Th isNumeric>Rank</Th>
-      </Tr>
-    </Thead>
-    <Tbody>
-      <Tr>
-        <Td>Python</Td>
-        <Td>Feature Engineering | EDA | Scikit-Learn | Tensorflow</Td>
-        <Td isNumeric>3</Td>
-      </Tr>
-      <Tr>
-        <Td>MERN</Td>
-        <Td>MongoDB | Express.js | React |  Node.js | Redux | Tailwind CSS | Docker | Git </Td>
-        <Td isNumeric>4</Td>
-      </Tr>
-      <Tr>
-        <Td>ML&DL</Td>
-        <Td>Natural Language Processing | Traditional Algorithms (Regressions and Classifications)</Td>
-        <Td isNumeric>3</Td>
-      </Tr>
-    </Tbody>
-    <Tfoot>
-      <Tr>
-        <Th>Other</Th>
-        <Th>Pytorch | MysSQL | PostgreSQL | Selenium | Matplotlib | Pandas | </Th>
-        <Th isNumeric>3</Th>
-      </Tr>
-    </Tfoot>
-  </Table>
-</TableContainer>
-    </div>
-  )
-}
+    <TableContainer>
+      <Table variant='striped' colorScheme='teal'>
+        <Thead>
+          <Tr>
+            <Th>Skills</Th>
+            <Th>Description</Th>
+            <Th isNumeric>Rank</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {skilledBlog.map(skill => (
+            <Tr key={skill.direct1}>
+              <Td>{skill.direct1}</Td>
+              <Td>{skill.direct2}</Td>
+              <Td isNumeric>{skill.direct3}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+};
 
 export default Skills
