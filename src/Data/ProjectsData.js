b3085761fcd491d70ae667ea8d2cfd757e66bc71
Doğuhan İlter <doguhannilt@gmail.com>
2024-08-11 07:38:42 +0300

    Update ProjectsData.js

--- src/Data/ProjectsData.js
+++ src/Data/ProjectsData.js
@@ -3,15 +3,18 @@ export const projects = [
         name: 'GloriousFood',
         description: 'A food recipe app that lets you find and save your favorite recipes.',
         image: 'https://www.prolificnorth.co.uk/wp-content/uploads/2013/02/uktv-food-glorious-food-logo_0.jpg',
+        link: 'github.com/doguhannilt/gloriousfood'
     },
     {
         name: 'Driver4U',
         description: 'A ride-sharing application that connects drivers with passengers.',
         image: 'https://raw.githubusercontent.com/Doguhannilt/Driver4U/master/images/logo.png',
+        link: 'github.com/doguhannilt/driver4u'
     },
     {
         name: 'WeirdStore',
         description: 'An online store with a unique collection of weird and wonderful items.',
         image: 'https://raw.githubusercontent.com/Doguhannilt/Weirdstore/master/frontend/src/images/login.png',
+        link: 'github.com/doguhannilt/weirdstore'
     },
 ];
