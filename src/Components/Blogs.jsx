9cbb3b8b75e4b9c9bde7877ec6b6ffa07c4a65aa
Doğuhan İlter <doguhannilt@gmail.com>
2024-08-11 20:55:01 +0300

    BLOGS UPDATE

--- src/Components/Blogs.jsx
+++ src/Components/Blogs.jsx
@@ -36,7 +36,7 @@ const Blogs = () => {
     return (
         <div className="bg-black py-10">
             <div className='mb-2 flex text-xl justify-center lg:text-3xl xl:font-bold xl:pt-4 font-mono cursor-pointer text-white'>
-                <div className='md:grid hidden md:grid-rows-2'>
+                <div className='grid  grid-rows-2'>
                     <span>READ MY BLOGS</span>
                 </div>
                 <span className='ml-2 cursor-pointer hidden md:block'>
@@ -59,10 +59,10 @@ const Blogs = () => {
                     >
                         <Link to={blog.to} className="block h-60 max-w-sm p-6 hover:h-80 bg-primary  hover:duration-300 duration-300 border-gray-200 rounded-lg shadow bg-transparent">
                             <div className="relative">
-                                <h5 className="mb-2 text-2xl font-bold  text-gray-900 dark:text-white">{blog.title}</h5>
-                                <p className="font-normal text-gray-700  dark:text-gray-400">{blog.description}</p>
+                                <h5 className="mb-2 text-2xl font-bold  text-white dark:text-white">{blog.title}</h5>
+                                <p className="font-normal text-white  dark:text-white">{blog.description}</p>
                                 <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center opacity-0 hover:opacity-100 transition-opacity duration-300">
-                                    <span className="bg-gray-600 text-white p-2 rounded-lg">View the details...</span>
+                                    <span className="bg-white text-white p-2 rounded-lg">View the details...</span>
                                 </div>
                             </div>
                         </Link>
