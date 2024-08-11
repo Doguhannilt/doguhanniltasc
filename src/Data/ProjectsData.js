149c13fad9a620e4d7dabf10d15480ff4009a97c
Doğuhan İlter <doguhannilt@gmail.com>
2024-08-11 07:30:36 +0300

    Update ProjectsData.js

--- src/Data/ProjectsData.js
+++ src/Data/ProjectsData.js
@@ -2,7 +2,7 @@ export const projects = [
     {
         name: 'GloriousFood',
         description: 'A food recipe app that lets you find and save your favorite recipes.',
-        image: 'https://private-user-images.githubusercontent.com/77373443/354841494-3ade490d-1855-4cbb-9af3-c9b251484f81.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjMzNDYzNjIsIm5iZiI6MTcyMzM0NjA2MiwicGF0aCI6Ii83NzM3MzQ0My8zNTQ4NDE0OTQtM2FkZTQ5MGQtMTg1NS00Y2JiLTlhZjMtYzliMjUxNDg0ZjgxLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA4MTElMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwODExVDAzMTQyMlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWM1YWFmOWY3YWY1NWU1YWIwZWU4NDZiODNmM2U5ZjQzOTk3MWE3ZjVkOWFhZTkyYzM0MjQwMmZiMjU3N2M5NjQmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.Uxa5mxv9CmMtTVsJlm8TgfJ3kI-1SfaFnClJlugQktU',
+        image: 'https://www.prolificnorth.co.uk/wp-content/uploads/2013/02/uktv-food-glorious-food-logo_0.jpg',
     },
     {
         name: 'Driver4U',
