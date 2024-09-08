1badd92b88fdf881a66a0d44534e4385ff34b410
doguhannilt <doguhannilt@gmail.com>
2024-09-08 13:34:29 +0300

    Bug Review

---
+++ src/pages/Cv_Card/Resume_Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+const Resume_Card = () => {
+  return (
+    <div className="hidden lg:rounded  lg:shadow-lg lg:grid grid-cols-3 h-80  w-full gap-10 ">
+
+    </div>
+  )
+}
+
+export default Resume_Card
